4978404698b4a3defaaa23c2d67482369346e2f2
Meenakshi Chauhan <meenakshichauhan@openlearning.com>
2026-09-02 13:02:30 +0000

    Disable username spellcheck

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -48,6 +48,7 @@ class HelloWorldApplicationTests {
                 .andExpect(content().contentTypeCompatibleWith("text/html"))
                 .andExpect(content().string(org.hamcrest.Matchers.containsString("Sign in")))
                 .andExpect(content().string(org.hamcrest.Matchers.containsString("autofocus")))
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("spellcheck=\"false\"")))
                 .andExpect(content().string(org.hamcrest.Matchers.containsString("aria-describedby=\"login-instructions\"")));
     }
 
